520aa91e9a3603a071d8a28befe84aa5d3aa1606
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-05-07 01:19:13 +0700

    Add check token middleware

---
+++ app/Http/Middleware/CheckToken.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CheckToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $token = $request->header('Token');
+        $time = date('Y-m-d H:i:s', mktime()-(5*60));
+        $check = DB::table('tokens')
+                    ->where('token', $token)
+                    ->where('updated_at', '>', $time);
+        if ($check->count() == 1) {
+            $token->update([
+                'updated_at' => date('Y-m-d H:i:s')
+            ]);
+            return $next($request);
+        } else {
+            $response403 = [
+                'code' => 403,
+                'description' => 'FORBIDDEN',
+                'message' => '',
+                'result' => ''
+            ];
+            return response(json_encode($response403, JSON_PRETTY_PRINT), 403);
+        }
+    }
+}
